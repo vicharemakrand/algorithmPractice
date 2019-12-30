ee88fec577509b0c01d4a336386243a191a3c1ae
mak <vichare_makrand@hotmail.com>
2019-12-30 15:09:02 +0000

    new example

---
+++ CodeInterview/CodeInterview/findMaxSubArray-kadane.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeInterview
+{
+    class findMaxSubArraykadane
+    {
+        // Function to find contiguous sub-array with the largest sum
+        // in given set of integers
+        static void Main(string[] args)
+        {
+             var arr = new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+ 
+            Console.WriteLine("find contiguous sub-array with the largest sum");
+            Console.WriteLine("Array  :" + string.Join(",", arr));
+ 
+            while (true) {
+                var input = Console.ReadLine();
+                (int maxSoFar, int start, int end) result = (0,0,0);
+                Console.ForegroundColor = ConsoleColor.White;
+                 if (input == "0")
+                {
+
+                    Console.WriteLine("exiting the app");
+                    break;
+                }
+                switch (input)
+                {
+                    case "1":
+                        {
+                            result= method1(arr);
+                            break;
+                        }
+                }
+
+                Console.WriteLine("The sum of contiguous sub-array with the " +
+                "largest sum is " + result.maxSoFar);
+                Console.Write("The contiguous sub-array with the largest sum is : ");
+                var final = arr.Where((o, i) => i >= result.start && i <= result.end).ToArray();
+                Console.Write(string.Join(",", final));
+            }
+
+        }
+
+ 
+        private static (int maxSoFar, int start, int end) method1(int[] arr)
+        {
+            // stores maximum sum sub-array found so far
+            int maxSoFar = 0;
+
+            // stores maximum sum of sub-array ending at current position
+            int maxEndingHere = 0;
+            // stores end-points of maximum sum sub-array found so far
+            int start = 0, end = 0;
+
+            // stores starting index of a positive sum sequence
+            int beg = 0;
+
+
+            // traverse the given array
+            for (int i=0; i < arr.Length;i++)
+            {
+                // update maximum sum of sub-array "ending" at index i (by adding
+                // current element to maximum sum ending at previous index i-1)
+                maxEndingHere = maxEndingHere + arr[i];
+
+                // if maximum sum is negative, set it to 0 (which represents
+                // an empty sub-array)
+                if (maxEndingHere < 0)
+                {
+                    maxEndingHere = 0;  // empty sub-array
+                    beg = i + 1;
+                }
+
+                // update result if current sub-array sum is found to be greater
+                if (maxSoFar < maxEndingHere)
+                {
+                    maxSoFar = maxEndingHere;
+                    start = beg;
+                    end = i;
+                }
+            }
+
+            return  ( maxSoFar, start, end );
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ CodeInterview/CodeInterview/replacearraywithproductofitems.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeInterview
+{
+    class replacearraywithproductofitems
+    {
+        // Recursive function to replace each element of the array with product
+        // of every other element without using division operator
+        static void Main(string[] args)
+        {
+             var arr = new int[] { 5, 3, 4, 2, 6, 8 };
+ 
+            Console.WriteLine("Recursive function to replace each element of the array with product");
+            Console.WriteLine("Array  :" + string.Join(",", arr));
+ 
+            while (true) {
+                var input = Console.ReadLine();
+                Console.ForegroundColor = ConsoleColor.White;
+                 if (input == "0")
+                {
+
+                    Console.WriteLine("exiting the app");
+                    break;
+                }
+                switch (input)
+                {
+                    case "1":
+                        {
+                            method1(arr);
+                            break;
+                        }
+                }
+            }
+        }
+
+ 
+        private static void method1(int[] arr)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            ReplacedArray(arr,0);
+            Console.WriteLine("Array  :" + string.Join(",", arr));
+        }
+
+        private static int ReplacedArray(int[] arr, int index)
+        {
+
+            var x= arr.Where((o, i) => i != index).Aggregate((product, item) => product * item);
+            if (index < arr.Length-1)
+            {
+                ReplacedArray(arr, index + 1);
+            }
+
+            arr[index] = x;
+            return x;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
